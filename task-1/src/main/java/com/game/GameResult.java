e20ab163b6257a11ba276657f3670f6db5c4419a
Pavel <p.shsherban@g.nsu.ru>
2026-04-07 16:41:01 +0700

    1

--- task-1/src/main/java/com/game/GameResult.java
+++
@@ -1,9 +0,0 @@
-package com.game;
-
-// Класс для хранения результата хода.
-public record GameResult(int bulls, int cows) {
-    @Override
-    public String toString() {
-        return String.format("%d быков, %d коров", bulls, cows);
-    }
-} 
